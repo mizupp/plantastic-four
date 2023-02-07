754920ce929bf1f4f9beb9e28b8ed67cd503da69
adamminchella <adamminchella@msn.com>
2023-02-07 11:27:31 +0000

    Begin search functionality

--- vite-project/src/components/SearchBar/index.jsx
+++ vite-project/src/components/SearchBar/index.jsx
@@ -1,42 +1,48 @@
-import React from 'react'
-import './styles.css'
+import axios, { all } from "axios";
+import React from "react";
+import { useState } from "react";
+import "./styles.css";
 
-const SearchBar = ({plants, setSearchResults }) => {
-const handleSubmit = (e) => e.preventDefault()
+const SearchBar = ({ loadResults }) => {
+  const [searchTerm, setSearchTerm] = useState("");
 
-const handleSearchChange = (e) => {
-    
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const headers = {
+      Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+    };
+    const allPlants = await axios.get("http://localhost:5000/plants", {
+      headers,
+    });
+    console.log(allPlants);
+    loadResults(allPlants);
+  };
+
+  const handleSearchChange = (e) => {
     const inputValue = e.target.value;
-    console.log(filterWord(inputValue));
+    setSearchTerm(inputValue);
+  };
 
-    if (!inputValue) {
-      return setSearchResults(plants) // If input is empty, display all plants
-    }
-    else {
-      const resultsArray = plants.filter(plant => plant.name.includes(filterWord(inputValue)))
-      setSearchResults(resultsArray)
-    }
-}
-
-const filterWord = (word) => {
-  let string = word;
-  let newString = string[0].toUpperCase() + string.slice(1);
-  return newString;
-}
-
+  const filterWord = (word) => {
+    let string = word;
+    let newString = string[0].toUpperCase() + string.slice(1);
+    return newString;
+  };
 
   return (
-    <div className='search-bar-comp'>
-        <form className='search' onSubmit={handleSubmit}>
-        <input className='search-input'
-        type="text"
-        name='searchValue'
-        id="search"
-        onChange={handleSearchChange} />
-        <button className='search-btn' />
-        </form>
+    <div className="search-bar-comp">
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          className="search-input"
+          type="text"
+          name="searchValue"
+          id="search"
+          onChange={handleSearchChange}
+        />
+        <input type="submit" value={"Search"} />
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default SearchBar;
